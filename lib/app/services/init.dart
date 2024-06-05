f85814dba7880364521d928fc6e21106c94bb5ea
md-ziaur-rahma <ziariyad018@gmail.com>
2024-06-05 13:42:55 +0600

    remove unuse code

--- lib/app/services/init.dart
+++ lib/app/services/init.dart
@@ -8,21 +8,9 @@ class AppInitializer {
     WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
     FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
     try {
-      await _initHive();
       sharedPreferences = await SharedPreferences.getInstance();
     } catch (e) {
       debugPrint("Error to initilize ${e.toString()}");
     }
-  }
-
-  Future<void> _initHive() async {
-    // await Hive.initFlutter();
-    // // Registering the home adapters
-    // Hive.registerAdapter(UnitModelAdapter());
-    // Hive.registerAdapter(UnitBookmarkModelAdapter());
-    // Hive.registerAdapter(ScanResultModelAdapter());
-
-    // Opening the box
-    // await Hive.openBox<HiveHomeModal>(AppHive.homeModalBox);
   }
 }
